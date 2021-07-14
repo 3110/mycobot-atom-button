ad2ff6437226432ac8075c55d72e250d1df363cd
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-07-15 00:44:13 +0900

    first version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# VSCode上のPlatformIO環境でMyCobotの開発をするための雛形
+# MyCobotのATOMのボタンの状態を取るサンプル
 
-MyCobotのプログラムをVSCode上のPlatformIO環境で書くための雛形です。
+[MyCobotのリポジトリ](https://github.com/elephantrobotics/myCobot/)のIssuesに挙がっていた「[How to read button on Atom](https://github.com/elephantrobotics/myCobot/issues/18#issuecomment-874576736)」のコメントにあったコードをベースにして，ATOMのボタンを押したときに，ATOMのLEDとBASICのLCDを同じ色で光らせるようにしています。
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,23 @@ void setup(void)
 {
     myCobot.setup();
     myCobot.powerOn();
+    pinMode(21, OUTPUT);
+    pinMode(22, OUTPUT);
+    myCobot.setPinMode(39, INPUT);
 }
 
 void loop(void)
 {
     M5.update();
+    if (myCobot.getDigitalInput(39) == 0)
+    {
+        M5.Lcd.fillScreen(TFT_GREEN);
+        myCobot.setLEDRGB(0x00, 0xff, 0x00);
+    }
+    else
+    {
+        M5.Lcd.fillScreen(TFT_BLACK);
+        myCobot.setLEDRGB(0x00, 0x00, 0x00);
+    }
+    delay(200);
 }
